b66ee01a0c2f5443f652476d155e8d4443102f32
Bouabidi <fethi.bouabidi@gmail.com>
2024-06-17 12:01:25 +0100

    load all courses

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,68 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
-import {CoursesService} from "../services/courses.service";
-import {Course, sortCoursesBySeqNo} from "../models/course.model";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
-import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  Injector,
+  signal,
+} from '@angular/core';
+import { CoursesService } from '../services/courses.service';
+import { Course, sortCoursesBySeqNo } from '../models/course.model';
+import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { CoursesCardListComponent } from '../courses-card-list/courses-card-list.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MessagesService } from '../messages/messages.service';
+import { catchError, from, throwError } from 'rxjs';
+import {
+  toObservable,
+  toSignal,
+  outputToObservable,
+  outputFromObservable,
+} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [
-    MatTabGroup,
-    MatTab,
-    CoursesCardListComponent
-  ],
+  imports: [MatTabGroup, MatTab, CoursesCardListComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  courses = signal<Course[]>([]);
+  coursesService = inject(CoursesService);
 
+  beginnerCourses = computed(() => {
+    // 1- define all signal dependencies
+    const courses = this.courses();
+    // 2- define the computation
+    return courses.filter((course) => {
+      course.category === 'BEGINNER';
+    });
+  });
+
+  advancedCourses = computed(() => {
+    const courses = this.courses();
+    return courses.filter((course) => {
+      course.category === 'ADVANCED';
+    });
+  });
+
+  constructor() {
+    effect(() => {
+      console.log(`Beginner courses: `, this.beginnerCourses())
+      console.log(`Advanced courses: `, this.advancedCourses())
+    });
+    this.loadCourses().then(() =>
+      console.log(`all courses loaded:`, this.courses)
+    );
+  }
+
+  async loadCourses() {
+    try {
+      const course = await this.coursesService.loadAllCourses();
+      this.courses.set(course.sort(sortCoursesBySeqNo));
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
