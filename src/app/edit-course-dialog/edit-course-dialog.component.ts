e047de7d210913d84b24f3e62195a85207c905fc
Bouabidi <fethi.bouabidi@gmail.com>
2024-06-18 09:03:04 +0100

    edit course dialog and form

--- src/app/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/edit-course-dialog/edit-course-dialog.component.ts
@@ -1,12 +1,19 @@
-import {Component, effect, inject, signal} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialog, MatDialogConfig, MatDialogRef} from "@angular/material/dialog";
-import {Course} from "../models/course.model";
-import {EditCourseDialogData} from "./edit-course-dialog.data.model";
-import {CoursesService} from "../services/courses.service";
-import {LoadingIndicatorComponent} from "../loading/loading.component";
-import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
-import {CourseCategoryComboboxComponent} from "../course-category-combobox/course-category-combobox.component";
-import {CourseCategory} from "../models/course-category.model";
+import { Component, effect, inject, signal } from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogConfig,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { Course } from '../models/course.model';
+import { EditCourseDialogData } from './edit-course-dialog.data.model';
+import { CoursesService } from '../services/courses.service';
+import { LoadingIndicatorComponent } from '../loading/loading.component';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { CourseCategoryComboboxComponent } from '../course-category-combobox/course-category-combobox.component';
+import { CourseCategory } from '../models/course-category.model';
+import { firstValueFrom } from 'rxjs';
+
 
 @Component({
   selector: 'edit-course-dialog',
@@ -14,12 +21,54 @@ import {CourseCategory} from "../models/course-category.model";
   imports: [
     LoadingIndicatorComponent,
     ReactiveFormsModule,
-    CourseCategoryComboboxComponent
+    CourseCategoryComboboxComponent,
   ],
   templateUrl: './edit-course-dialog.component.html',
-  styleUrl: './edit-course-dialog.component.scss'
+  styleUrl: './edit-course-dialog.component.scss',
 })
 export class EditCourseDialogComponent {
+  dialogRef = inject(MatDialogRef);
+  data: EditCourseDialogData = inject(MAT_DIALOG_DATA);
 
+  fb= inject(FormBuilder);
 
+  form = this.fb.group({
+    title: [''],
+    longDescription: [''],
+    iconUrl: ['']
+  });
+
+  courseService= inject(CoursesService);
+
+  category = signal<CourseCategory>('BEGINNER');
+
+  constructor(){
+    this.form.patchValue({
+      title: this.data?.course?.title,
+      longDescription: this.data?.course?.longDescription,
+      iconUrl: this.data?.course?.iconUrl
+    });
+    this.category.set(this.data?.course?.category??"BEGINNER");
+    effect(()=>{
+      console.log(`Course Category: ${this.category()}`);
+    })
+  }
+
+  onClose(){
+    this.dialogRef.close();
+  }
+}
+
+export async function openEditCourseDialog(
+  dialog: MatDialog,
+  data: EditCourseDialogData
+) {
+  const config = new MatDialogConfig();
+  config.disableClose = true;
+  config.autoFocus = true;
+  config.width = '400px';
+  config.data = data;
+
+  const close$ = dialog.open(EditCourseDialogComponent, config).afterClosed();
+  return firstValueFrom(close$);
 }
