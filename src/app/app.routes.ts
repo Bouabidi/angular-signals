e3f02d15f83b7620790889750f1c20bd001da3b7
Bouabidi <fethi.bouabidi@gmail.com>
2024-06-19 16:26:16 +0100

    add course component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,24 @@ import {Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {LessonsComponent} from "./lessons/lessons.component";
+import { CourseComponent } from './course/course.component';
+import { courseLessonsResolver } from './course/course-lessons.resolver';
+import { courseResolver } from './course/course.resolver';
+import { isUserAuthenticated } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    'path':'courses/:courseId',
+    component:CourseComponent,
+    canActivate: [isUserAuthenticated],
+    resolve: {
+      course: courseResolver,
+      lessons: courseLessonsResolver
+    }
   },
   {
     path: "login",
